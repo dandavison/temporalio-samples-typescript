fce7b2345d8b81b653aa20350c16f9f5304d6b90
Dan Davison <dan.davison@temporal.io>
2024-02-24 18:32:55 -0500

    queue

---
+++ queue/queue.ts
@@ -0,0 +1,84 @@
+import { inWorkflowContext, defineSignal, defineQuery, setHandler, condition } from '@temporalio/workflow';
+import { Connection, Client, WorkflowIdReusePolicy } from '@temporalio/client';
+import { Worker, DefaultLogger, Runtime } from '@temporalio/worker';
+
+const put = defineSignal<[string]>('put');
+const get = defineQuery('get');
+
+export async function workflow(name: string): Promise<void> {
+  const queue: string[] = [];
+
+  setHandler(put, (s: string) => {
+    queue.push(s);
+  });
+  setHandler(get, () => queue.shift());
+
+  await condition(() => false); // TODO: ContinueAsNew
+}
+
+async function starter(workflowId: string, taskQueue: string, client: Client): Promise<void> {
+  const result = await client.workflow.execute(workflow, {
+    taskQueue,
+    workflowId,
+    args: ['Temporal'],
+    workflowIdReusePolicy: WorkflowIdReusePolicy.WORKFLOW_ID_REUSE_POLICY_TERMINATE_IF_RUNNING,
+  });
+  console.log(result);
+}
+
+interface Args {
+  workflowId: string;
+  taskQueue: string;
+}
+
+async function main(args: Args): Promise<void> {
+  const worker = await Worker.create({
+    workflowsPath: __filename,
+    taskQueue: args.taskQueue,
+    bundlerOptions: {
+      ignoreModules: ['@temporalio/client', '@temporalio/worker'],
+    },
+  });
+  const connection = await Connection.connect();
+  const client = new Client({ connection });
+  await worker.runUntil(starter(args.workflowId, args.taskQueue, client));
+}
+
+if (!inWorkflowContext()) {
+  Runtime.install({ logger: new DefaultLogger('WARN') });
+  main(getArgs()).catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
+}
+
+function getArgs(): Args {
+  let workflowId = '';
+  let taskQueue = '';
+  let argv = process.argv.slice(2);
+  while (argv.length) {
+    const arg = argv.shift()!;
+    if (['-w', '--workflow-id'].includes(arg)) {
+      workflowId = argv.shift()!;
+    } else if (['-t', '--task-queue'].includes(arg)) {
+      taskQueue = argv.shift()!;
+    } else {
+      die(`unrecognized argument: ${arg}`);
+    }
+  }
+  if (!(workflowId && taskQueue)) {
+    die('');
+  }
+  return { workflowId, taskQueue };
+}
+
+function die(message: string) {
+  console.error(`${message}
+  Required arguments:
+  -w, --workflow-id  workflow ID
+  -t, --task-queue   task queue
+
+  Example:
+  ${process.argv.slice(0, 2).join(' ')} -w my-workflow-id -t my-task-queue`);
+  process.exit(1);
+}
